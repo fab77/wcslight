cc9ec07e9434f8b84add91663508b475d7e1b583
fab77 <fabrizio.giordano@ext.esa.int>
2022-10-09 17:56:19 +0200

    code cleaning ... a bit

--- test/USE_CASE_3.integration.spec.js
+++ test/USE_CASE_3.integration.spec.js
@@ -148,24 +148,24 @@ function test04() {
 }
 
 
-// console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-// console.log("Running test 1/4 of USE CASE 2")
-// console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-// test01();
-// console.log("@")
-// console.log("@")
-// console.log("@")
-// console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-// console.log("Running test 2/4 of USE CASE 2")
-// console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-// test02();
-// console.log("@")
-// console.log("@")
-// console.log("@")
-// console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-// console.log("Running test 3/4 of USE CASE 2")
-// console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-// test03();
+console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+console.log("Running test 1/4 of USE CASE 2")
+console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+test01();
+console.log("@")
+console.log("@")
+console.log("@")
+console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+console.log("Running test 2/4 of USE CASE 2")
+console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+test02();
+console.log("@")
+console.log("@")
+console.log("@")
+console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+console.log("Running test 3/4 of USE CASE 2")
+console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+test03();
 console.log("@")
 console.log("@")
 console.log("@")
